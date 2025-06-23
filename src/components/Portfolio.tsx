76027b1ea5bb8b65a1556082ae98ce7771042a33
reinaldojunior3108 <reinaldo310805@edu.unipar.br>
2025-06-23 13:42:35 -0300

    responsividade

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -10,7 +10,7 @@ export default function Portfolio() {
                     {/* --- CARD 1 CORRIGIDO --- */}
                     {/* Adicionado: h-auto md:h-full aspect-video md:aspect-auto */}
                     <div className="bg-[url(https://ik.imagekit.io/pagnofotografia/IMG_9446.jpg)] relative bg-cover bg-center w-full md:w-[25%] h-[60dvh] md:h-full aspect-video md:aspect-auto flex items-center justify-center rounded-[30px] shadow-[3px_6px_15px_rgba(255,255,255,0.2),inset_6px_4px_6px_0px_rgba(255,_255,_255,_0.1)]">
-                        <div className="opacity-60 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
+                        <div className="opacity-60 md:opacity-0 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
                             <a href="https://instagram.com/pagnofotografia"><h1 className="flex justify-center items-center h-full font-bold text-[24px]">ENSAIOS</h1></a>
                         </div>
                     </div>
@@ -18,7 +18,7 @@ export default function Portfolio() {
                     {/* --- CARD 2 CORRIGIDO --- */}
                     {/* Adicionado: h-auto md:h-full aspect-video md:aspect-auto */}
                     <div className="bg-[url(https://ik.imagekit.io/pagnofotografia/IMG_0382-1.jpg)] relative bg-cover bg-center w-full md:w-[25%] h-[60dvh] md:h-full aspect-video md:aspect-auto flex items-center justify-center rounded-[30px] shadow-[3px_6px_15px_rgba(255,255,255,0.2),inset_6px_4px_6px_0px_rgba(255,_255,_255,_0.1)]">
-                        <div className=" opacity-60 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
+                        <div className=" opacity-60 md:opacity-0 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
                             <a href="https://instagram.com/pagnofotografia"><h1 className="flex justify-center items-center h-full font-bold text-[24px]">CARROS</h1></a>
                         </div>
                     </div>
@@ -26,7 +26,7 @@ export default function Portfolio() {
                     {/* --- CARD 3 CORRIGIDO --- */}
                     {/* Adicionado: h-auto md:h-full aspect-video md:aspect-auto */}
                     <div className="bg-[url(https://ik.imagekit.io/pagnofotografia/IMG_0018-6.jpg)] relative bg-cover bg-center w-full md:w-[25%] h-[60dvh] md:h-full aspect-video md:aspect-auto flex items-center justify-center rounded-[30px] shadow-[3px_6px_15px_rgba(255,255,255,0.2),inset_6px_4px_6px_0px_rgba(255,_255,_255,_0.1)]">
-                        <div className="opacity-60 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
+                        <div className="opacity-60 md:opacity-0 rounded-[30px] bg-black md:hover:opacity-70 h-full w-full transition-opacity absolute">
                             <a href="https://instagram.com/pagnofotografia"><h1 className="flex justify-center items-center h-full font-bold text-[24px]">EVENTOS</h1></a>
                         </div>
                     </div>
